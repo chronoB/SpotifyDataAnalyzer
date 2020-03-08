fabca3d00cfd8c2f9309cc1969ec412c4c736a32
chronoB <finn.o.bayer@gmail.com>
2020-03-08 22:38:26 +0100

    [add] added example file for functionality

---
+++ example.py
@@ -0,0 +1,70 @@
+from src.analyzer import Analyzer
+
+
+def main():
+    analyzer = Analyzer(["./data/example/testUser/StreamingHistory.json"])
+
+    print("Example 1: getPopularArtists()")
+    pa = analyzer.getPopularArtists()
+    print(pa)
+
+    print()
+    print("Example 1a: getPopularArtists() with specific Daytime")
+    payload = {"daytime": "morning"}
+    pa = analyzer.getPopularArtists(payload=payload)
+    print(pa)
+
+    print()
+    print("Example 1b: getPopularArtists() of Month July")
+    payload = {"month": 7}
+    pa = analyzer.getPopularArtists(payload=payload)
+    print(pa)
+
+    print()
+    print("Example 1c: getPopularArtists() of Month July for podcasts")
+    payload = {"month": 7, "media": "podcast"}
+    pa = analyzer.getPopularArtists(payload=payload)
+    print(pa)
+
+    print()
+    print(
+        "Example 1d: getPopularArtists() of specified period (2019-03-03 to 2019-03-04) for music, count = 3"
+    )
+    payload = {
+        "startYear": 2019,
+        "startMonth": 3,
+        "startDay": 3,
+        "endYear": 2019,
+        "endMonth": 3,
+        "endDay": 4,
+        "count": 3,
+        "media": "music",
+    }
+    pa = analyzer.getPopularArtists(payload=payload)
+    print(pa)
+
+    print()
+    print("Example 2: getPopularItems()")
+    pi = analyzer.getPopularItems()
+    print(pi)
+
+    print()
+    print("Example 2a: getPopularItems() for podcasts")
+    payload = {
+        "media": "podcast",
+    }
+    pi = analyzer.getPopularItems(payload=payload)
+    print(pi)
+
+    print()
+    print("Example 2b: getPopularItems() for podcasts by time played in ms")
+    payload = {
+        "media": "podcast",
+        "ratingCrit": "time",
+    }
+    pi = analyzer.getPopularItems(payload=payload)
+    print(pi)
+
+
+if __name__ == "__main__":
+    main()
