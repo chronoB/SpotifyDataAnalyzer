e696198118f8f89ab93206f09c86d5dc98b090c2
chronoB <finn.o.bayer@gmail.com>
2020-03-09 19:41:39 +0100

    [update,fix] added entrypoints, requirements and fixes

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
 
-from setuptools import setup, find_packages
 from os import path
+
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -24,14 +25,14 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     keywords="spotify data analysis",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=3.5",
-    install_requires=[""],  # TODO: Add requirements as they come
+    packages=find_packages(include=["src", "src.*"]),
+    python_requires=">=3.6",
+    install_requires=["voluptuous==0.11.7"],
+    tests_require=["pytest"],
+    entry_points={"console_scripts": ["spotifydataanalyzer=example:main"]},
 )
