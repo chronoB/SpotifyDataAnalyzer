a04fb59cf2076d1a68ae3c3c236421262333cbdd
chronoB <finn.o.bayer@gmail.com>
2020-11-03 21:38:15 +0100

    made the language parameter optional

--- podcast_fetcher.py
+++ podcast_fetcher.py
@@ -93,14 +93,14 @@ if __name__ == "__main__":
     if conn is not None:
         # create podcast table
         create_table(conn, sql_create_podcasts_table)
-        language = sys.argv[1]
+
         for letter in ascii_lowercase:
-            url = (
-                "https://api.spotify.com/v1/search?type=show&limit=50&q="
-                + letter
-                + "&market="
-                + language
-            )
+            url = "https://api.spotify.com/v1/search?type=show&limit=50&q=" + letter
+
+            if len(sys.argv) > 1:
+                language = sys.argv[1]
+                url + "&market=" + language
+
             print("Populating database for letter: " + letter)
             populateDatabase(url)
 
