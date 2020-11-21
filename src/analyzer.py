1ce847ca4cb37846afc3d8f8e11f5f94b6c96095
chronoB <finn.o.bayer@gmail.com>
2020-11-21 18:49:00 +0100

    added type annotations

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,12 +3,13 @@ import json
 import operator
 import sqlite3
 from datetime import datetime
+from typing import Dict, List, Sequence, Tuple, Union
 
 from .schemas import *
 
 
 class Analyzer:
-    def __init__(self, streamingHistoryFiles=[]):
+    def __init__(self, streamingHistoryFiles: List[str] = []) -> None:
         """Load the data from the given StreamingHistory files into a dictionary for lookups.
         Every item will have an extra "user"-feature added that contains the name of the folder where the StreamingHistory is located.
         Example of default value:
@@ -28,17 +29,21 @@ class Analyzer:
             streamingHistoryFiles = ["./data/example/testUser/StreamingHistory.json"]
         self._initDatabaseConnection()
         self.libraryFiles = streamingHistoryFiles
-        self.library = list()
+        self.library: List[Dict[str, str]] = list()
         self._fetchItemsFromLibraryFiles()
         return
 
-    def getPopularArtists(self, payload={}):
+    def getPopularArtists(
+        self, payload: Dict[str, Union[int, str]] = {}
+    ) -> List[Tuple[str, int]]:
         return self._getPopular("artistName", payload=payload)
 
-    def getPopularItems(self, payload={}):
+    def getPopularItems(
+        self, payload: Dict[str, Union[int, str]] = {}
+    ) -> List[Tuple[str, int]]:
         return self._getPopular("trackName", payload=payload)
 
-    def getNumberOfItems(self, payload={}):
+    def getNumberOfItems(self, payload: Dict[str, Union[int, str]] = {}) -> int:
         searchSpecs, keyword, media, count, ratingCrit = self._extractSearchSpecs(
             payload
         )
@@ -55,46 +60,60 @@ class Analyzer:
             numOfItems += 1
         return numOfItems
 
-    def getDataPerWeekday(self, dataType="number", weekdayFormat=0):
-        if type(weekdayFormat) == int:
+    def getDataPerWeekday(
+        self, dataType: str = "number", weekdayFormat: Union[int, str] = 0
+    ) -> Dict[Union[str, int], object]:
+        if isinstance(weekdayFormat, int):
             return self._getDataPer("weekday", weekdayNumbers, dataType)
-        elif type(weekdayFormat) == str:
+        elif isinstance(weekdayFormat, str):
             return self._getDataPer("weekday", weekdayNames, dataType)
+        else:
+            raise Exception("weekdayFormat is not of type int or str")
 
-    def getDataPerDayTime(self, dataType="number"):
+    def getDataPerDayTime(
+        self, dataType: str = "number"
+    ) -> Dict[Union[str, int], object]:
         return self._getDataPer("daytime", daytimeList, dataType)
 
-    def getDataPerYear(self, dataType="number"):
-        yearList = self._getListOfPossibleYears()
+    def getDataPerYear(self, dataType: str = "number") -> Dict[Union[str, int], object]:
+        yearList: List[int] = self._getListOfPossibleYears()
         return self._getDataPer("year", yearList, dataType)
 
-    def getDataPerMonth(self, dataType="number"):
-        monthList = [x for x in range(1, 13)]
+    def getDataPerMonth(
+        self, dataType: str = "number"
+    ) -> Dict[Union[str, int], object]:
+        monthList: List[int] = [x for x in range(1, 13)]
         return self._getDataPer("month", monthList, dataType)
 
-    def getDataPerDay(self, dataType="number"):
-        dayList = [x for x in range(1, 32)]
+    def getDataPerDay(self, dataType: str = "number") -> Dict[Union[str, int], object]:
+        dayList: List[int] = [x for x in range(1, 32)]
         return self._getDataPer("day", dayList, dataType)
 
-    def _getDataPer(self, key, timeList, dataType):
+    def _getDataPer(
+        self, key: str, timeList: Sequence[Union[str, int]], dataType: str
+    ) -> Dict[Union[str, int], object]:
         dt = DataType(dataType)
         retDict = {}
         for time in timeList:
+
             data = {
                 "number": self.getNumberOfItems(payload={key: time}),
                 "artist": self.getPopularArtists(payload={key: time}),
                 "item": self.getPopularItems(payload={key: time}),
             }.get(dt, [])
+
             retDict[time] = data
         return retDict
 
-    def _getPopular(self, key, payload={}):
+    def _getPopular(
+        self, key: str, payload: Dict[str, Union[int, str]] = {}
+    ) -> List[Tuple[str, int]]:
         searchSpecs, keyword, media, count, ratingCrit = self._extractSearchSpecs(
             payload
         )
 
-        popular = {}
-        sorted_popular = []
+        popular: Dict[str, int] = {}
+        sorted_popular: List[Tuple[str, int]] = []
 
         for item in self.library:
             if not (
@@ -111,7 +130,7 @@ class Analyzer:
 
         return sorted_popular
 
-    def _fetchItemsFromLibraryFiles(self):
+    def _fetchItemsFromLibraryFiles(self) -> None:
         for fileName in self.libraryFiles:
             with open(fileName, encoding="utf-8") as jsonFile:
                 tmpList = json.load(jsonFile)
@@ -124,18 +143,18 @@ class Analyzer:
 
                 self.library.extend(tmpList)
 
-    def _checkIfPodcast(self, name):
+    def _checkIfPodcast(self, name: str) -> int:
         cursor = self.databaseConnection.cursor()
         cursor.execute("SELECT count(*) FROM podcasts WHERE name = ?", (name,))
         data = cursor.fetchone()[0]
         if data == 0:
-            return False
+            return 0
         else:
-            return True
+            return 1
 
-    def _getUsername(self, fileName):
+    def _getUsername(self, fileName: str) -> str:
         # Returns the username from a given filename
-        def getNthSlash(n):
+        def getNthSlash(n: int) -> int:
             if n == 0:
                 return 0
             return fileName.find("/", getNthSlash(n - 1)) + 1
@@ -143,13 +162,15 @@ class Analyzer:
         idxOfSlash = getNthSlash(3)
         return fileName[idxOfSlash : fileName.rfind("/")]
 
-    def _itemInTimeslot(self, searchSpecs, item):
+    def _itemInTimeslot(
+        self, searchSpecs: SearchSpecifics, item: Dict[str, str]
+    ) -> int:
         if searchSpecs == {}:
-            return True
+            return 1
 
         if "weekday" in searchSpecs:
             if not self._itemInTimeslot_Weekday(searchSpecs["weekday"], item):
-                return False
+                return 0
 
         if "daytime" in searchSpecs:
             return self._itemInTimeslot_Daytime(searchSpecs["daytime"], item)
@@ -167,10 +188,12 @@ class Analyzer:
             return self._itemInTimeslot_Time(searchSpecs, item)
 
         else:
-            return True
+            return 1
 
-    def _itemInTimeslot_Time(self, searchSpecs, item):
-        isInTimeslot = True
+    def _itemInTimeslot_Time(
+        self, searchSpecs: SearchSpecifics, item: Dict[str, str]
+    ) -> int:
+        isInTimeslot = 1
 
         for spec, value in searchSpecs.items():
             res = {
@@ -178,13 +201,15 @@ class Analyzer:
                 "month": value == int(item["endTime"][5:7]),
                 "day": value == int(item["endTime"][8:10]),
                 "hour": value == int(item["endTime"][11:13]),
-            }.get(spec, True)
+            }.get(spec, 1)
             if not res:
-                isInTimeslot = False
+                isInTimeslot = 0
         return isInTimeslot
 
-    def _itemInTimeslot_Period(self, searchSpecs, item):
-        isInTimeslot = True
+    def _itemInTimeslot_Period(
+        self, searchSpecs: SearchSpecifics, item: Dict[str, str]
+    ) -> int:
+        isInTimeslot = 1
         startDate = datetime(
             searchSpecs["startYear"],
             searchSpecs["startMonth"],
@@ -201,22 +226,25 @@ class Analyzer:
             datetime.fromisoformat(item["endTime"]) < startDate
             or datetime.fromisoformat(item["endTime"]) > endDate
         ):
-            isInTimeslot = False
+            isInTimeslot = 0
 
         return isInTimeslot
 
-    def _itemInTimeslot_Daytime(self, spec, item):
+    def _itemInTimeslot_Daytime(self, spec: str, item: Dict[str, str]) -> int:
         hour = int(item["endTime"][11:13])
         return {
             "night": hour >= 0 and hour < 6,
             "morning": hour >= 6 and hour < 12,
             "afternoon": hour >= 12 and hour < 18,
             "evening": hour >= 18 and hour <= 23,
-        }.get(spec, False)
+        }.get(spec, 0)
 
-    def _itemInTimeslot_Weekday(self, spec, item):
-        if str == type(spec):
-            spec = {
+    def _itemInTimeslot_Weekday(
+        self, spec: Union[str, int], item: Dict[str, str]
+    ) -> int:
+
+        if isinstance(spec, str):
+            intSpec = {
                 "monday": 0,
                 "tuesday": 1,
                 "wednesday": 2,
@@ -225,16 +253,25 @@ class Analyzer:
                 "saturday": 5,
                 "sunday": 6,
             }.get(spec)
-        return datetime.weekday(datetime.fromisoformat(item["endTime"])) == spec
+        elif isinstance(spec, int):
+            intSpec = spec
+        return datetime.weekday(datetime.fromisoformat(item["endTime"])) == intSpec
 
-    def _itemIsMedia(self, media, item):
-        return {
-            "all": True,
-            "podcast": item["podcast"],
-            "music": not item["podcast"],
-        }.get(media, False)
+    def _itemIsMedia(self, media: str, item: Dict[str, str]) -> int:
+        isMedia = {
+            "all": 1,
+            "podcast": int(item["podcast"]),
+            "music": not int(item["podcast"]),
+        }.get(media, 0)
+        return isMedia
 
-    def _addItemToList(self, item, usedDict, key, ratingCrit):
+    def _addItemToList(
+        self,
+        item: Dict[str, str],
+        usedDict: Dict[str, int],
+        key: str,
+        ratingCrit: Union[str, int],
+    ) -> Dict[str, int]:
         if key == "artistName":
             if item[key] not in usedDict.keys():
                 usedDict[item[key]] = (
@@ -257,7 +294,9 @@ class Analyzer:
                 )
         return usedDict
 
-    def _extractSearchSpecs(self, payload):
+    def _extractSearchSpecs(
+        self, payload: Dict[str, Union[int, str]]
+    ) -> Tuple[Dict[str, int], str, str, int, str]:
         searchSpecs = SearchSpecifics(payload)
         media = (
             Media("all") if not "media" in searchSpecs else Media(searchSpecs["media"])
@@ -271,7 +310,7 @@ class Analyzer:
         keyword = "" if not "keyword" in searchSpecs else searchSpecs["keyword"]
         return (searchSpecs, keyword, media, count, ratingCrit)
 
-    def _getListOfPossibleYears(self):
+    def _getListOfPossibleYears(self) -> List[int]:
         listOfYears = []
         for item in self.library:
             year = int(item["endTime"][:4])
@@ -280,13 +319,15 @@ class Analyzer:
 
         return listOfYears
 
-    def _itemHasKeyword(self, keyword, item):
-        if keyword.lower() in item["artistName"].lower():
-            return True
-        if keyword.lower() in item["trackName"].lower():
-            return True
+    def _itemHasKeyword(self, keyword: str, item: Dict[str, str]) -> int:
+        if (
+            keyword.lower() in item["artistName"].lower()
+            or keyword.lower() in item["trackName"].lower()
+        ):
+            return 1
+        return 0
 
-    def _initDatabaseConnection(self):
+    def _initDatabaseConnection(self) -> None:
         db_file = "./data/podcasts.db"
         try:
             self.databaseConnection = sqlite3.connect(db_file)
